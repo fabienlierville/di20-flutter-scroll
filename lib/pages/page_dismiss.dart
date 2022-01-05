9cace44ec31b0bad65e920a01694fe41ad41d65c
Fabien LIERVILLE <flierville@cesi.fr>
2022-01-05 10:03:06 +0100

    PageDismiss

---
+++ lib/pages/page_dismiss.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:scroll/models/activite.dart';
+
+class PageDismiss extends StatefulWidget {
+  const PageDismiss({Key? key}) : super(key: key);
+
+  @override
+  _PageDismissState createState() => _PageDismissState();
+}
+
+class _PageDismissState extends State<PageDismiss> {
+  ScrollController _controller = ScrollController();
+
+  List<Activite> activites = [
+    Activite(nom: "Vélo", icone: Icons.directions_bike),
+    Activite(nom: "Peinture", icone: Icons.palette),
+    Activite(nom: "Golf", icone: Icons.golf_course),
+    Activite(nom: "Arcade", icone: Icons.gamepad),
+    Activite(nom: "Bricolage", icone: Icons.build),
+    Activite(nom: "Vélo", icone: Icons.directions_bike),
+    Activite(nom: "Peinture", icone: Icons.palette),
+    Activite(nom: "Golf", icone: Icons.golf_course),
+    Activite(nom: "Arcade", icone: Icons.gamepad),
+    Activite(nom: "Bricolage", icone: Icons.build),
+    Activite(nom: "Vélo", icone: Icons.directions_bike),
+    Activite(nom: "Peinture", icone: Icons.palette),
+    Activite(nom: "Golf", icone: Icons.golf_course),
+    Activite(nom: "Arcade", icone: Icons.gamepad),
+    Activite(nom: "Bricolage", icone: Icons.build),
+    Activite(nom: "Vélo", icone: Icons.directions_bike),
+    Activite(nom: "Peinture", icone: Icons.palette),
+    Activite(nom: "Golf", icone: Icons.golf_course),
+    Activite(nom: "Arcade", icone: Icons.gamepad),
+    Activite(nom: "Bricolage", icone: Icons.build),
+    Activite(nom: "Vélo", icone: Icons.directions_bike),
+    Activite(nom: "Peinture", icone: Icons.palette),
+    Activite(nom: "Golf", icone: Icons.golf_course),
+    Activite(nom: "Arcade", icone: Icons.gamepad),
+    Activite(nom: "Bricolage", icone: Icons.build),
+    Activite(nom: "Vélo", icone: Icons.directions_bike),
+    Activite(nom: "Peinture", icone: Icons.palette),
+    Activite(nom: "Golf", icone: Icons.golf_course),
+    Activite(nom: "Arcade", icone: Icons.gamepad),
+    Activite(nom: "Bricolage", icone: Icons.build),
+  ];
+
+  @override
+  void initState() {
+    _controller.addListener(listener);
+    super.initState();
+  }
+
+  void listener(){
+    print("Postiion = ${_controller.position.pixels} | Max = ${_controller.position.maxScrollExtent}");
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("List Tile"),
+        ),
+        body: ListView.separated(
+          itemCount: activites.length,
+          controller: _controller,
+          itemBuilder: (context, index){
+            return ListTile(
+              title: Text("Activité:"),
+              subtitle: Text(activites[index].nom),
+              trailing: Icon(activites[index].icone),
+              leading: Icon(activites[index].icone),
+              onTap: (){
+                print(activites[index].nom);
+              },
+            );
+          },
+          separatorBuilder: (context, index){
+            if(index % 5 == 0){
+              return Divider(color: Colors.red,);
+            }
+            return Container();
+
+          },
+        )
+    );
+  }
+
+}
