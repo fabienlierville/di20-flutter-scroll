4b71e8dcb80a3987fd0b9821858c41cd6ffadca3
Fabien LIERVILLE <flierville@cesi.fr>
2022-01-05 11:00:13 +0100

    PageGridOrientation

--- lib/pages/page_grid_orientation.dart
+++ lib/pages/page_grid_orientation.dart
@@ -72,11 +72,14 @@ class _PageGridOrientationState extends State<PageGridOrientation> {
         itemCount: activites.length,
         itemBuilder: (context, index){
           Activite activite = activites[index];
-          return GridTile(
-            header: Text("Activité", textAlign: TextAlign.center,),
-            child: Icon(activite.icone, size: 40,),
-            footer: Text(activite.nom, textAlign: TextAlign.center, style: TextStyle(color: Colors.blue),),
-          );
+          return Card(
+            elevation: 3.0,
+            child: GridTile(
+              header: Text("Activité", textAlign: TextAlign.center,),
+              child: Icon(activite.icone, size: 40,),
+              footer: Text(activite.nom, textAlign: TextAlign.center, style: TextStyle(color: Colors.blue),),
+            ),
+          ) ;
         }
     );
 
